ad28de4f48fe32ef45555bfe1ee1041a4440ebac
Swetha Venagla <swethavengala555@gmail.com>
2017-02-18 20:02:08 -0600

    Added Modal popup templates for Login/Register button and Shopping Cart in both mobile and Desktop versions.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	var itemsSelected = 0;
 
 	$("#navBar").load("partials/nav-bar.html");
+	$("#loginModal").load("partials/login.html");
+	$("#cartModal").load("partials/cart.html");
 	$("#items-list").load("partials/items-list.html", function(argument) {
 		$(".item").click(function() {
 		itemsSelected++;
@@ -12,4 +14,10 @@ $(document).ready(function() {
 	});
 	$("#footer").load("partials/footer.html");
 	
+	$(".login-action").click(function() {
+	 	$('#login-modal').modal('show');
+	});
+	$(".cart-action").click(function() {
+	 	$('#cart-modal').modal('show');
+	});
 });
